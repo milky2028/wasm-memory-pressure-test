f295a678fcf470cc7a90852d2317e9309e90798a
Tyler <tylergross28@gmail.com>
2024-04-11 12:00:27 -0400

    current progress

---
+++ server.ts
@@ -0,0 +1,46 @@
+import { toReadableStream } from "https://deno.land/std/io/mod.ts";
+
+const port = 8080;
+const handler = async (request: Request): Promise<Response> => {
+  const url = new URL(request.url);
+
+  switch (url.pathname) {
+    case "/dist/allocate.debug.wasm": {
+      const body = toReadableStream(
+        await Deno.open("./dist/allocate.debug.wasm")
+      );
+      return new Response(body, {
+        status: 200,
+        headers: {
+          "Content-Type": "application/wasm",
+        },
+      });
+    }
+
+    case "/dist/allocate.debug.js": {
+      const body = toReadableStream(
+        await Deno.open("./dist/allocate.debug.js")
+      );
+      return new Response(body, {
+        status: 200,
+        headers: {
+          "Content-Type": "text/javascript",
+        },
+      });
+    }
+
+    default: {
+      const body = toReadableStream(await Deno.open("./index.html"));
+      return new Response(body, {
+        status: 200,
+        headers: {
+          "Cross-Origin-Opener-Policy": "same-origin",
+          "Cross-Origin-Embedder-Policy": "require-corp",
+        },
+      });
+    }
+  }
+};
+
+console.log(`HTTP server running. Access it at: http://localhost:8080/`);
+Deno.serve({ port }, handler);
---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include <cstdlib>
+#include <cstring>
+
+template <typename T>
+size_t to_mb(T num) {
+  return num * 1024 * 1024;
+}
+
+template <typename T>
+T int_to_ptr(intptr_t ptr) {
+  return reinterpret_cast<T>(ptr);
+}
+
+template <typename T>
+intptr_t ptr_to_int(T ptr) {
+  return reinterpret_cast<intptr_t>(ptr);
+}
+
+emscripten::val get_buffer(intptr_t buffer_ptr, size_t buffer_size) {
+  return emscripten::val(emscripten::typed_memory_view<uint8_t>(to_mb(buffer_size), int_to_ptr<uint8_t*>(buffer_ptr)));
+}
+
+intptr_t allocate_memory(size_t to_allocate) {
+  auto size = to_mb(to_allocate);
+  void* allocated_memory = malloc(size);
+  memset(allocated_memory, '$', size);
+  return ptr_to_int(allocated_memory);
+}
+
+EMSCRIPTEN_BINDINGS() {
+  emscripten::function("allocate_memory", &allocate_memory);
+  emscripten::function("get_buffer", &get_buffer, emscripten::allow_raw_pointers());
+}
